37b4f1291fd43b4bfb37be4e925918b34b43fa18
leedd87 <leedav87@gmail.com>
2022-08-10 14:41:04 -0300

    portfolio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,12 @@ const Home = () => {
 	return (
 		<div className="bg-blue-200 min-h-screen grid place-content-center">
 			<div className="w-[500px] flex justify-center flex-col items-center">
-				<h1>DAVID LEE</h1>
+				<h1 className="text-4xl">DAVID LEE</h1>
 				<div className=" w-[250px] my-5 flex items-center justify-around">
-					<LinkRouter to="/english" className="btn-eng">
+					<LinkRouter to="/english" className="btn-engEn">
 						EN
 					</LinkRouter>
-					<LinkRouter to="/espanol" className="btn-eng">
+					<LinkRouter to="/espanol" className="btn-engEs">
 						ES
 					</LinkRouter>
 				</div>
